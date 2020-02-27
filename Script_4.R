d809068ad10c5456110b5679bea5f7d3fefd628b
Athziri Charles <Charlesathziri@hotmail.com>
2020-02-27 09:25:03 -0600

    Prueba T

---
+++ Script_4.R
@@ -0,0 +1,20 @@
+
+
+
+CR <- read.csv("Clases/CedroRojo.csv", header = T)
+summary(CR)
+
+
+# Una muestra -------------------------------------------------------------
+
+# Media teorica de la variable Diametro
+# Establecida por CONAFOR para cedr rojo es igual a 13
+
+# Comparar la media observada de la variable Diamtero para las plantulas producidas en vivero
+# (media=12.524, valores observados)
+
+# "mu" debe ser igual a la variable terica
+# el valor de alfa establecido es 0.05
+
+t.test(CR$diametro, mu=13)
+
